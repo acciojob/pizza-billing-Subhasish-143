3d779cac06ad96531c8f273bd7005756388f4111
Subhasish Padhee <44895248+Subhasish-143@users.noreply.github.com>
2022-10-09 13:09:04 +0530

    Update DeluxePizza.java

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,12 +5,7 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        if (isVeg) {
-            total += 150;
-        }
-        else {
-            total += 200;
-        }
-        setBill("Base Price Of The Pizza: "+total+"\n");
+        addExtraCheese();
+        addExtraToppings();
     }
 }
