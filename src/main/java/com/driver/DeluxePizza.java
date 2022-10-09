273af34827f4e2ec81b1c60f570664eee6924523
Subhasish-143 <subhasish.padhee@gmail.com>
2022-10-09 12:22:33 +0530

    added every method

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,12 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+        if (isVeg) {
+            total += 150;
+        }
+        else {
+            total += 200;
+        }
+        setBill("Base Price Of The Pizza: "+total+"\n");
     }
 }
